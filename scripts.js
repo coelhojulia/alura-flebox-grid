c7f7ef06eb98cef512656093a4908bed9ec24f76
juisabelly48@gmail.com <juisabelly@gmail.com>
2022-12-20 16:00:33 -0300

    Finalização do projeto.

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const botaoMenu = document.querySelector('.cabecalho-menu')
-const menu = document.querySelector('.menu-lateral')
-
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-lateral--ativo')
-})
